361637fb27816b7cbe5a7b944ab508d1c885fee4
Aaron Chang <aaronchang153@gmail.com>
2019-11-06 13:48:15 -0800

    Tentative pinout definitions

---
+++ src/ghousedefs.h
@@ -0,0 +1,18 @@
+#ifndef _GHOUSEDEFS_H_
+#define _GHOUSEDEFS_H_
+
+namespace pinout
+{
+    static const char SERIAL_DEV[] = "/dev/ttyS0";
+
+    namespace adafruit_feather
+    {
+        static const int BAUD_RATE = 9600;
+        static const int TX_PIN = 15;
+        static const int RX_PIN = 16;
+        static const int TX_LEN = 1;
+        static const int RX_LEN = 8;
+    };
+};
+
+#endif //_GHOUSEDEFS_H_
