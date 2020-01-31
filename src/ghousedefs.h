03887c9f17657bff184bdcacc318eb8fa3d68dea
Aaron Chang <aaronchang153@gmail.com>
2020-01-30 23:03:35 -0800

    Added tentative definitions for leds and pumps

--- src/ghousedefs.h
+++ src/ghousedefs.h
@@ -10,6 +10,35 @@ namespace pinout
         static const int BAUD_RATE = 9600;
         static const unsigned char REQUEST_SIG = 0;
     };
+
+    namespace led
+    {
+        static const int PIN = 20; // placeholder
+        static const int ON = 1;
+        static const int OFF = 0;
+    };
+
+    namespace pump
+    {
+        namespace ph_up
+        {
+            static const int PIN = 21; // placeholder
+            static const int ON = 1;
+            static const int OFF = 0;
+        };
+        namespace ph_down
+        {
+            static const int PIN = 22; // placeholder
+            static const int ON = 1;
+            static const int OFF = 0;
+        };
+        namespace nutrition
+        {
+            static const int PIN = 23; // placeholder
+            static const int ON = 1;
+            static const int OFF = 0;
+        };
+    };
 };
 
 #endif //_GHOUSEDEFS_H_
