c4f2cbb0edfba7bc860999d2274f9887684d722b
Tun Myat <tunmyat.bryan@gmail.com>
2020-02-03 18:28:44 -0800

    Add defs for pins and thresholds

--- src/ghousedefs.h
+++ src/ghousedefs.h
@@ -3,6 +3,14 @@
 
 static const char DATA_LOG_FILE[] = "data.log";
 
+static const float PH_THRESH_LOW = 5.8;
+static const float PH_THRESH_HIGH = 6.3;
+
+static const float EC_THRESH_LOW = 600.0;
+static const float EC_THRESH_HIGH = 1000.0;
+
+static const int DATA_SAMPLE_TIME_MS = 60000;
+
 namespace pinout
 {
     static const char SERIAL_DEV[] = "/dev/ttyUSB0";
@@ -15,7 +23,7 @@ namespace pinout
 
     namespace led
     {
-        static const int PIN = 20; // placeholder
+        static const int PIN = 26; // placeholder
         static const int ON = 1;
         static const int OFF = 0;
     };
